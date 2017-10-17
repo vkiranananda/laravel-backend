d8ef732081eb34ad505b603709122dc35b1cb66c
vkiranananda <kiran@light-joy.ru>
2017-10-17 18:11:09 +0300

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,45 +1,26 @@
 <?php
 
-use Backend\Root\Core\Services\Backend;
-
-Backend::installRoutes('Sitemap');
+Backend::installRoutes('Sitemap', 'Root');
 
 Route::group(['prefix' => 'control', 'middleware' => 'auth.basic'], function()
 {
 	Route::get('/', '\Backend\Root\Home\Controllers\HomeController@admin');
-<<<<<<< HEAD
-	Backend::installRoutes('User\Root');
-	Backend::installRoutes('Option\Root', 'Backend', true);
-=======
-	Backend::installRoutes('User');
-	Backend::installRoutes('Option', 'Backend', true);
->>>>>>> 335b97e178203b3721db194e913a3e19b7c70ee0
-	Backend::installRoutes('Ad', 'Backend', true);
+	Backend::installRoutes('User', 'Root');
+	Backend::installRoutes('Option', 'Root', true);
 });
 
 Route::group(['prefix' => 'content', 'middleware' => 'auth.basic'], function()
 {
 	Route::get('/', '\Backend\Root\Home\Controllers\HomeController@content');
 
-<<<<<<< HEAD
-	Backend::installRoutes('Category', 'Backend\Root', true);
-	Backend::installRoutes('Page', 'Backend\Root', true);
-	Backend::installRoutes('News', 'Backend\Root', true);
-	Backend::installRoutes('MediaFile', 'Backend\Root', true);
-	Backend::installRoutes('Property', 'Backend', true);
-	Backend::installRoutes('Excursion', 'Backend', true);
-	Backend::installRoutes('Realty', 'Backend', true);
-	Backend::installRoutes('Upload\Root');
-=======
-	Backend::installRoutes('Category', 'Backend', true);
-	Backend::installRoutes('Page', 'Backend', true);
-	Backend::installRoutes('News', 'Backend', true);
-	Backend::installRoutes('MediaFile', 'Backend', true);
-	Backend::installRoutes('Property', 'Backend', true);
-	Backend::installRoutes('Excursion', 'Backend', true);
-	Backend::installRoutes('Realty', 'Backend', true);
-	Backend::installRoutes('Upload');
->>>>>>> 335b97e178203b3721db194e913a3e19b7c70ee0
+	Backend::installRoutes('Category', 'Root', true);
+	Backend::installRoutes('Page', 'Root', true);
+	Backend::installRoutes('News', 'Root', true);
+	Backend::installRoutes('MediaFile', 'Root', true);
+	Backend::installRoutes('Property', 'Root', true);
+	Backend::installRoutes('Excursion', 'Root', true);
+	Backend::installRoutes('Realty', 'Root', true);
+	Backend::installRoutes('Upload', 'Root');
 });
 
 Route::group(['prefix' => 'utils', 'middleware' => 'auth.basic'], function()
@@ -47,10 +28,14 @@ Route::group(['prefix' => 'utils', 'middleware' => 'auth.basic'], function()
 	Route::get('/', '\Backend\Root\Home\Controllers\HomeController@utils');
 	Route::get('routes', '\Backend\Root\Info\Controllers\InfoController@routes');
 	
-	Backend::installRoutes('Redirect');
+	Backend::installRoutes('Redirect', 'Root');
 });
 
 Categories::installRoutes();
+
+
+
+
 
 
 
