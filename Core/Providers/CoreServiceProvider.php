57ad9653ac8682099772eb75d57a707f7808e952
vkiranananda <kiran@light-joy.ru>
2017-10-17 18:16:09 +0300

    Update CoreServiceProvider.php

--- Core/Providers/CoreServiceProvider.php
+++ Core/Providers/CoreServiceProvider.php
@@ -34,17 +34,5 @@ class CoreServiceProvider extends ServiceProvider
         {
             return new \Backend\Root\Upload\Services\UploadedFiles();
         });
-<<<<<<< HEAD
-=======
-
-        AliasLoader::getInstance()->alias('BackendConfig', '\Backend\Root\Core\Facades\ConfigFacade');
-        AliasLoader::getInstance()->alias('Backend', '\Backend\Root\Core\Facades\BackendFacade');
-        AliasLoader::getInstance()->alias('UploadedFiles', '\Backend\Root\Upload\Facades\UploadedFilesFacade');
-        AliasLoader::getInstance()->alias('Categories', '\Backend\Root\Category\Facades\CategoriesFacade');
-        AliasLoader::getInstance()->alias('Content', '\Backend\Root\Site\Services\Content');
-        AliasLoader::getInstance()->alias('Helpers', '\Backend\Root\Core\Services\Helpers');
-        AliasLoader::getInstance()->alias('Forms', '\Backend\Root\Form\Services\Forms');
-
->>>>>>> 335b97e178203b3721db194e913a3e19b7c70ee0
     }
 }
