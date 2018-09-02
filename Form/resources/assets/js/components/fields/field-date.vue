73cab536c247b5766701bf98b54cf4d64c206a6c
Kiran <kiran@light-joy.ru>
2018-09-02 17:00:49 +0000

    init

--- Form/resources/assets/js/components/fields/field-date.vue
+++ Form/resources/assets/js/components/fields/field-date.vue
@@ -1,14 +1,72 @@
 <template>
-    <input v-bind="data.attr" :type="data.type" :value="data.value" :name="data.name">
+    <div>
+    	<date-picker  :input-class="inputClass" v-model="date" @change="changeDate" :input-name="name" :first-day-of-week="1" :format="format" :lang="lang"></date-picker>
+    	<date-picker :input-class="inputClass" v-model="dateTo" :not-before="notBefore" :input-name="nameTo" :first-day-of-week="1"  :format="format" v-if="range" :lang="lang"></date-picker>
+	</div>
 </template>
 
 <script>
-    export default {
-        props: [ 'data' ],
-        created () {
-        //    this.$bus.$on('UploadDeleteFile', this.fileDeleted);
-          //  this.$bus.$on('UploadAttacheFiles', this.attacheFiles)
-          console.log('123');
-        },
+import DatePicker from 'vue2-datepicker'
+import fecha from 'fecha'
+
+export default {
+	props: {
+	  data: {
+	  	default: () => [],
+	  	// type: Array
+	  },
+	},
+	created() {
+		console.log(this.data);
+		if(this.data.value != undefined && this.data.value != '') 
+			this.date = this.parceDate(this.data.value);
+		if(this.data['value-to'] != undefined && this.data['value-to'] != '') 
+			this.dateTo = this.parceDate(this.data['value-to']);
+	
+		if(this.data.attr != undefined) {
+			if (this.data.attr.class != undefined)this.inputClass += ' '+this.data.attr.class;
+		} 
+		if(this.data.format != undefined)this.format = this.data.format;
+
+		if(this.data.name != undefined)this.name = this.data.name;
+		if(this.data.nameTo != undefined)this.nameTo = this.data['name-to'];
+
+		if(this.data.range != undefined)this.range = true;
+
+	},
+  components: { DatePicker },
+  data() {
+    return {
+      date: '',
+      dateTo: '',
+      lang: 'ru',
+      notBefore: '',
+
+      range: false,
+      inputClass: 'mx-input form-control',
+      format: 'DD.MM.YYYY',
+	  name: 'date',
+	  nameTo: 'dateTo'
     }
-</script>
+  },
+    methods: {
+	    changeDate: function (time) {
+	    	if(this.range) {
+	    		this.notBefore = time;
+	    		console.log(this.dateTo, '11');
+	    		if(this.dateTo != '' && this.dateTo < this.date){
+	    			this.dateTo = this.date;
+	    		}
+	    	}
+
+	    },
+		parceDate (date) {
+		  try {
+		    return fecha.parse(date, this.format)
+		  } catch (e) {
+		    return ''
+		  }
+		}
+  }
+}
+</script> 
