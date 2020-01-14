f87dcf482c5c321d82cd42f2ccc8b66f6d5423b4
Kiran <kiran@light-joy.ru>
2020-01-14 13:32:41 +0000

    init

--- Category/Controllers/RouteController.php
+++ Category/Controllers/RouteController.php
@@ -11,18 +11,30 @@ class RouteController
 
 		$url = '/'.$url;
 
-		if(isset($routes[$url]) ){
+		// Категории, точное совпадение
+		if (isset($routes[$url]) ){
 			$cat = $routes[$url];
 			$class = Categories::getModuleControllers(key($cat))['categoryController'];
 			$c = new $class;
 			return $c->index(reset($cat));
-		}else{
-			$catUrl = "/";
-			if( preg_match("/(^.*)\/(.+)$/", $url, $urlExt) && isset($routes[$urlExt[1]]) ) {
-				$cat = $routes[$urlExt[1]];
-				$class = Categories::getModuleControllers(key($cat))['viewControllerAction'];
-				$c = new $class;
-				return $c->show(reset($cat), $urlExt[2]);
+		} else {
+			// Записи внутри, если записи с данным урлом нет, проверяем запись с вложенным урлом
+			$resUrl = '';
+			while (preg_match("/^(.*)\/(.+)$/", $url, $urlExt)) {
+				// Весь урл до последнего сегмента
+				$url = $urlExt[1];
+				// Создаем конечный путь, может быть вложенным
+				$resUrl = ($resUrl != '') ? $urlExt[2] . "/" . $resUrl : $urlExt[2];
+
+				// Если урл есть 
+				if (isset($routes[$url])) {
+					$cat = $routes[$url];
+					// Корнева каотеогрия, получаем контроллер
+					$class = Categories::getModuleControllers(key($cat))['viewControllerAction'];
+					$c = new $class;
+
+					return $c->show(reset($cat), $resUrl);
+				}
 			}
 		}
 		abort(404);
