cfae6c4825ad906404588a13166b9bf27af0cf68
Kiran <kiran@light-joy.ru>
2020-05-12 15:04:46 +0300

    init

--- Form/resources/js/components/fields/checkbox.vue
+++ Form/resources/js/components/fields/checkbox.vue
@@ -1,6 +1,6 @@
 <template functional>
-	<div class="form-check" :class="props.field.inline? 'form-check-inline': ''">
-		<label v-for="el in props.field.options" class="form-check-label mr-2" :key="el.value" v-on:change="listeners.change($event.target.value)" :class='props.error ? "is-invalid" : "" '>
+	<div class="form-check" :class="props.field.inline ? 'form-check-inline': ''">
+		<label v-for="el in props.field.options" class="form-check-label mr-2" :key="el.value" v-on:change="console.log($event.target.value); listeners.change($event.target.value)" :class='props.error ? "is-invalid" : "" '>
     		&nbsp;<input v-bind="props.field.attr" type="checkbox" :value="props.field.value[el.value]" :checked="el.value == props.field.value">
     		{{el.label}}
     	</label>
