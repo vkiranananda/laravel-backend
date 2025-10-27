11ed6f0f77f8da910f43f5c5aa3a5c1238c922fd
kiran <kiran@light-joy.ru>
2025-10-27 19:57:30 +0530

    Обновлён компонент group.vue: добавлен вывод метки поля, улучшена структура кода. Добавлены новые стили для временных и датовых полей в style.scss.

--- Form/resources/js/components/form/group.vue
+++ Form/resources/js/components/form/group.vue
@@ -1,27 +1,27 @@
 <template>
-    <fields-list  :fields='field.fields' :errors="errors" :store-name='storeName' v-bind="field.attr"></fields-list>
+    <label v-if="field.label" v-html="field.label"></label>
+    <fields-list :fields="field.fields" :errors="errors" :store-name="storeName" v-bind="field.attr"></fields-list>
 </template>
 
 <script>
-    // import fieldsList2 from '../form/fields.vue'
-    export default {
-        components: {
-            // 'fields-list': fieldsList2, 
+// import fieldsList2 from '../form/fields.vue'
+export default {
+    components: {
+        // 'fields-list': fieldsList2,
+    },
+    props: {
+        field: {},
+        error: undefined,
+        storeName: {
+            type: String,
+            default: '',
         },
-        props: { 
-        	field: {},
-        	error: undefined,
-            storeName: {
-                type: String,
-                default: ''
-            }
+    },
+    computed: {
+        errors: function () {
+            if (this.error == undefined) return {};
+            return this.error;
         },
-        computed: {
-            errors: function () {
-                if (this.error == undefined) return {};
-                return this.error;
-            }
-        },
-    }
+    },
+};
 </script>
-
